a193273fbfbf0c2e8060854841118396b6e471e6
Lewism177 <maylewis@claflin.edu>
2026-09-11 09:40:35 -0400

    Add files via upload
    phylogenetic-Analysis.R

---
+++ Phylogenetic-Analysis.R
@@ -0,0 +1,13 @@
+#Author: Maya, Date:09/11/2026,Purpose: Phyologenetic analysis
+
+#Load the 4 libraries
+library(ape)
+library(phangorn)
+library(phytools)
+library(geiger)
+"A,B,C,D,Z"
+# Create some dummy atring dataset
+
+text.string<- "(((((((cow,pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark);" 
+
+vert.tree<-read.tree(text=text.string)
